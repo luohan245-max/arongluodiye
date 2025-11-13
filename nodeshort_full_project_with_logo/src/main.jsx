0c0dc58f2569b8cb5802a13dc74e56f244e5103b
luohan245-max <luohan245@gmail.com>
2025-11-13 12:59:18 +0700

    Delete nodeshort_full_project_with_logo directory

--- nodeshort_full_project_with_logo/src/main.jsx
+++
@@ -1,9 +0,0 @@
-
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import NodeshortLeadPage from './NodeshortLeadPage'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <NodeshortLeadPage />
-)
